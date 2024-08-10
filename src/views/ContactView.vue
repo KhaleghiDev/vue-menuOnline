9fd509268567990a5c5a316d86ff72163407c501
khaleghidev <abolfazlkhaleghi1474@yahoo.com>
2024-08-10 15:15:27 +0330

    edit cintact us add link and style

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="contact">
-    <h1>This is an contact us page</h1>
+    <h1>تماس با ما</h1>
+    <hr />
+    <ul class="contact-list">
+      <li class="contact-item"><a href="tel:05138426248">ارتباط با دفتر مشهد</a></li>
+      <li class="contact-item"><a href="tel:09002488881">ارتباط با پشتیبان</a></li>
+      <li class="contact-item"><a href="https://wa.me/989002488881">واتساپ</a></li>
+      <li class="contact-item"><a href="https://t.me/edutsn_admin">تلگرام</a></li>
+      <li class="contact-item"><a href="https://www.facebook.com/edutsn">فیسبوک</a></li>
+      <li class="contact-item"><a href="https://www.instagram.com/edu.tsn/">اینستاگرام</a></li>
+    </ul>
+
   </div>
 </template>
 
@@ -10,6 +20,26 @@
     min-height: 100vh;
     display: flex;
     align-items: center;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    align-content: center;
+    justify-content: center;
+    text-align: center;
+}
+  .contact   h1 {
+    border-bottom: 2px solid #ccc;
+    line-height: 2.3em;
   }
+  .contact ul li:first-child {
+    margin-top: 25px;
+  }
+  .contact ul li{
+    line-height: 2rem;
+  }
+  .contact ul li a {
+    margin-bottom: 10px;
+    padding: 5px 7px;
+  }
+  
 }
 </style>
