9e5ae776b7e96855071786cbf496695e3b1405bb
khaleghidev <abolfazlkhaleghi1474@yahoo.com>
2024-08-10 15:14:22 +0330

    edit style about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>درباره ما</h1>
+    <p class="text-about">
+      مهاجرت، سفر و تحصیل به خارج از کشور، ویزا، موضوعاتی هستند که این روزها به
+      شنیدن آنها عادت کرده ایم. تیم ما در کنار شماست تا تمامی مراحل این مسیر را
+      در کوتاه ترین زمان برای شما انجام دهد؛ با کمترین هزینه و به بهترین شکل در
+      شرکت طلوع سفیران نور در یک تیم 20 نفره همراه شما هستیم. خدمات ما در زمینه
+      مهاجرت به تمامی کشورهای پرطرفدار دنیا در زمینه های مهاجرت تحصیلی است و در
+      حال حاضر در بیش از 30 دانشگاه معتبر جهان ارائه خدمات و پذیرش دانشجو داریم.
+    </p>
   </div>
 </template>
 
@@ -10,6 +18,17 @@
     min-height: 100vh;
     display: flex;
     align-items: center;
+    flex-direction: column;
+    justify-content: center;
+  }
+  .about h1 {
+    border-bottom: 2px solid #ccc;
+    line-height: 2.3em;
+  }
+  .text-about {
+    padding: 10px 5px;
+    text-align: right;
+    line-height: 1.7;
   }
 }
 </style>
